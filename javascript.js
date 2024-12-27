55f7630f77b18930e945051ce11c23f42a08c7d9
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2024-12-27 13:44:38 +0100

    corrections

--- javascript.js
+++ javascript.js
@@ -1,20 +1,3 @@
-/*function togglemode_sombre() {
-    const mode_sombre = document.querySelector("#modeSombre");
-    const body = document.querySelector("body");
-
-    if (mode_sombre) {
-        function toggleModeSombre() {
-        body.classList.toggle("sombre");
-        }
-        mode_sombre.addEventListener("click", toggleModeSombre);
-    }
-    }
-
-window.addEventListener("DOMContentLoaded", function () {
-    togglemode_sombre();
-    });
-*/
-
 function toggleDarkMode() {
     document.body.classList.toggle("sombre");
     }
